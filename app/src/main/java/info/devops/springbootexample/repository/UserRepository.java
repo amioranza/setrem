136ea14fffb9bf507c05eaa78851f161f8ed5883
Alexandre Mioranza <amioranza@mdcnet.ninja>
2020-03-26 19:29:17 -0300

    Feat: rebuild

--- app/src/main/java/info/devops/springbootexample/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package info.devops.springbootexample.repository;
-
-import info.devops.springbootexample.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
